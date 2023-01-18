edecde094221715f8fff05577451aaa121ee580e
ducnguyen <minhducnguyen189@gmail.com>
2023-01-18 12:41:19 +0700

    update example with component scan for spring inversion of control

---
+++ spring-inversion-of-control-annotation/src/main/java/com/spring/core/spring/inversion/of/control/annotation/SpringApplication.java
@@ -0,0 +1,7 @@
+package com.spring.core.spring.inversion.of.control.annotation;
+
+public class SpringApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
