4b658d1a9b4a89766493b60c45e11fc5221b9679
ducnguyen <minhducnguyen189@gmail.com>
2023-03-13 23:30:04 +0700

    update example

---
+++ spring-mvc-xml-configuration/src/main/java/com/spring/core/spring/mvc/xml/configuration/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.spring.core.spring.mvc.xml.configuration.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    @RequestMapping("/welcome")
+    public String welcome(ModelMap modelMap) {
+        modelMap.addAttribute("name", "Duc");
+        return "welcome";
+    }
+
+}
